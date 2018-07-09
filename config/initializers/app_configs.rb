51ddc2107719277e511537c7989f739c34ac8130
Mostafa Zahran <mostafa.k.zahran@gmail.com>
2018-07-09 21:52:14 +0300

    handle application yml not found error

--- config/initializers/app_configs.rb
+++ config/initializers/app_configs.rb
@@ -1,4 +1,8 @@
 require 'yaml'
 
-yaml_data = YAML::load(ERB.new(IO.read(File.join(Rails.root, 'config', 'application.yml'))).result)
-yaml_data.each{|key, value| ENV[key] = value}
+begin
+  yaml_data = YAML::load(ERB.new(IO.read(File.join(Rails.root, 'config', 'application.yml'))).result)
+  yaml_data.each {|key, value| ENV[key] = value}
+rescue
+  p 'application.yml not found'
+end
